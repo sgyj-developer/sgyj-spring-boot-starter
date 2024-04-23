8dbb63276095a33ae04cd96b87cbf4f1762b707d
이승구 <leesg107@naver.com>
2024-04-23 22:21:53 +0900

    feat: first commit

---
+++ src/main/java/com/yeseung/sgyjspringbootstarter/SgyjSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.yeseung.sgyjspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgyjSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgyjSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeseung/sgyjspringbootstarter/SgyjSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeseung.sgyjspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgyjSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
