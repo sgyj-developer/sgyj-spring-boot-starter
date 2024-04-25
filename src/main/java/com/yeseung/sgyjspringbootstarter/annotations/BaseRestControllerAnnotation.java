7ab1ca30484a9b99cd58c30773d232d32cf43a0f
이승구 <leesg107@naver.com>
2024-04-25 11:13:26 +0900

    feat: add restcontroller annotation

--- src/main/java/com/yeseung/sgyjspringbootstarter/annotations/BaseRestControllerAnnotation.java
+++ src/main/java/com/yeseung/sgyjspringbootstarter/annotations/BaseRestControllerAnnotation.java
@@ -1,2 +1,21 @@
-package com.yeseung.sgyjspringbootstarter.annotations;public @interface BaseRestControllerAnnotation {
+package com.yeseung.sgyjspringbootstarter.annotations;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@RestController
+@RequestMapping
+public @interface BaseRestControllerAnnotation {
+
+    @AliasFor(annotation = RestController.class)
+    String value() default "";
+
+    @AliasFor(annotation = RequestMapping.class)
+    String path() default "";
+
 }
