a21374174e4a89c19936dbdbd59e333c25abef3e
이승구 <leesg107@naver.com>
2024-04-26 21:17:36 +0900

    feat: active configurationproperties

--- src/main/java/com/yeseung/sgyjspringbootstarter/SgyjSpringBootStarterApplication.java
+++ src/main/java/com/yeseung/sgyjspringbootstarter/SgyjSpringBootStarterApplication.java
@@ -2,8 +2,10 @@ package com.yeseung.sgyjspringbootstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "com.yeseung")
 public class SgyjSpringBootStarterApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/yeseung/sgyjspringbootstarter/properties/BasePathProperties.java
+++ src/main/java/com/yeseung/sgyjspringbootstarter/properties/BasePathProperties.java
@@ -1,2 +1,13 @@
-package com.yeseung.sgyjspringbootstarter.properties;public class BasePathProperties {
+package com.yeseung.sgyjspringbootstarter.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter @Setter
+@ConfigurationProperties("base.path")
+public class BasePathProperties {
+
+    private String uploadDir;
+
 }
